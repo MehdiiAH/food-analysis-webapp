77177d4b40b2e5b491590eec89c07562acf10c60
julienNiol <julienniol@free.fr>
2025-10-21 11:59:28 +0200

    Adding logs and exception for NLP functions

--- src/food_analysis/core/nlp.py
+++ src/food_analysis/core/nlp.py
@@ -4,12 +4,47 @@ The module contains functions to execute a search based on NLP using TF-IDF
 """
 
 import json
+import logging
+from logging.handlers import RotatingFileHandler
 from typing import Dict, List, Tuple
 
 import numpy as np
 from scipy.sparse import csr_matrix, save_npz
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.neighbors import NearestNeighbors
+
+# Messages de log à différents niveaux
+logging.debug("Ceci est un message de niveau DEBUG")
+logging.info("Ceci est un message de niveau INFO")
+logging.warning("Ceci est un message de niveau WARNING")
+logging.error("Ceci est un message de niveau ERROR")
+logging.critical("Ceci est un message de niveau CRITICAL")
+
+# Création d'un logger pour ce module
+logger = logging.getLogger(__name__)
+
+# Définir le niveau de log sur INFO
+logger.setLevel(logging.INFO)
+
+# Création d'un handler permettant la rotation des logs, avec format compréhensible
+
+file_handler = RotatingFileHandler(
+    "nlp.log", maxBytes=5 * 1024 * 1024, backupCount=5, encoding="utf-8"
+)
+file_handler.setLevel(logging.INFO)
+handler_format = logging.Formatter(
+    "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+file_handler.setFormatter(handler_format)
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(handler_format)
+console_handler.setLevel(logging.INFO)
+
+# Éviter d'ajouter plusieurs handlers si le module est importé plusieurs fois
+if not logger.handlers:
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
 
 
 def create_vocabulary(texts: List[str]) -> Dict:
@@ -28,11 +63,17 @@ def create_vocabulary(texts: List[str]) -> Dict:
         The dictionary of words to be counted
 
     """
+    if not texts:
+        logger.error("The list of tokens is empty")
+        raise ValueError("Missing word list")
+
     words: set[str] = set()
     for t in texts:
         words = words.union(set(t))
     n_features = len(words)
     vocabulary = dict(zip(words, range(n_features)))
+
+    logger.info("Vocabulary created")
 
     return vocabulary
 
@@ -59,6 +100,10 @@ def count_words(texts: List[str], vocabulary: Dict) -> csr_matrix:
     n_samples = len(texts)
     n_features = len(vocabulary)
 
+    if (n_samples == 0) or (n_features == 0):
+        logger.error("Words or Vocabulary is missing")
+        raise ValueError("Missing data")
+
     counts = np.zeros((n_samples, n_features))
 
     # Filling the matrix by iterating over the documents and counting the words
@@ -70,6 +115,8 @@ def count_words(texts: List[str], vocabulary: Dict) -> csr_matrix:
     counts_sparse = csr_matrix(counts)
 
     save_vocabulary_counts(counts_sparse, vocabulary)
+
+    logger.info("Vocabulary and bag of words saved")
 
     return counts_sparse
 
@@ -96,6 +143,9 @@ def count_query(tokens: List[str], vocabulary: Dict) -> csr_matrix:
     """
     count_vector = np.zeros(len(vocabulary), dtype=int)
 
+    if not tokens:
+        logger.warning("Search string empty. Results may be unconsistent")
+
     # Filling the matrix by iterating over the documents and counting the words
     for word in tokens:
         if word in vocabulary:
@@ -103,6 +153,8 @@ def count_query(tokens: List[str], vocabulary: Dict) -> csr_matrix:
 
     # Creating a sparse matrix for TF-IDF transformation
     counts_sparse = csr_matrix(count_vector)
+
+    logger.info("Search string added in the count vector")
 
     return counts_sparse
 
@@ -133,7 +185,9 @@ def tf_idf_search(
     tf_idf_recipe = transformer.fit_transform(recipe_counts)
     tf_idf_query = transformer.transform(query_counts)
 
-    # Ensure we always return a CSR sparse matrix (fit_transform may return ndarray)
+    logger.info("TF-IDF realized")
+
+    # Ensure returning a CSR sparse matrix for type hinting (fit_transform may return ndarray)
     if isinstance(tf_idf_recipe, csr_matrix):
         if isinstance(tf_idf_query, csr_matrix):
             return tf_idf_recipe, tf_idf_query
@@ -172,7 +226,9 @@ def knn_search(
     """
     knn_model = NearestNeighbors(n_neighbors=k, metric="cosine", algorithm="brute")
     knn_model.fit(tf_idf)
+    logger.info("KNN model trained")
     distances, indices = knn_model.kneighbors(query_input)
+    logger.info("Closest recipes predicted")
 
     return distances, indices
 
